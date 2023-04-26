c8e454501ef15f103593f3c1ed833beeb482a697
Racharla Saikumar <rsaikumar1999@gmail.com>
2023-04-26 06:45:21 +0000

    “cgetstore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [search, setSearch] = useState("");
+  const [bio, setBio] = useState({ id: "", name: "", company: "" });
+
+  const getName = (event) => {
+    setName(event.target.value);
+  };
+
+  const getId = (event) => {
+    setId(event.target.value);
+  };
+
+  const getCompany = (event) => {
+    setCompany(event.target.value);
+  };
+  const getSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const submitData = async (event) => {
+    event.preventDefault();
+    const userDetails = { id, name, company };
+    const url = "/submit/";
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userDetails),
+    };
+    const response = await fetch(url, options);
+
+    const data = await response.json();
+    console.log(data.status);
+    setId("");
+    setName("");
+    setCompany("");
+  };
+
+  //   useEffect(() => {
+  //     const getName = async () => {
+  //       const response = await fetch("/users/");
+  //       const data = await response.json();
+  //       console.log(data);
+  //     };
+
+  //     getName();
+  //   }, []);
+
+  const getDetails = async () => {
+    const url = `/users/${search}/`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setBio({
+      id: data.id,
+      name: data.name,
+      company: data.company,
+    });
+    console.log(bio);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={submitData}>
+        <input
+          type="text"
+          value={id}
+          onChange={getId}
+          placeholder="company id"
+        />
+        <br />
+        <input
+          type="text"
+          value={name}
+          onChange={getName}
+          placeholder="username"
+        />
+        <br />
+        <input
+          type="text"
+          value={company}
+          onChange={getCompany}
+          placeholder="company name"
+        />
+        <br />
+        <button type="submit">submit</button>
+      </form>
+      <input
+        type="text"
+        value={search}
+        onChange={getSearch}
+        placeholder="search by id"
+      />
+      <br />
+      <button type="button" onClick={getDetails}>
+        search
+      </button>
+      {bio.id !== "" && (
+        <div>
+          <p>company id: {bio.id}</p>
+          <p>username: {bio.name}</p>
+          <p>company: {bio.company}</p>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default App;
